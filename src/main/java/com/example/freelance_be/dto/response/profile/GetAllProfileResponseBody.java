c09c06dea9ad93db005c72604a1458c2a212ea86
mrvntech <mrvntech@gmail.com>
2023-12-02 17:11:39 +0700

    fix profile

--- src/main/java/com/example/freelance_be/dto/response/profile/GetAllProfileResponseBody.java
+++ src/main/java/com/example/freelance_be/dto/response/profile/GetAllProfileResponseBody.java
@@ -14,6 +14,16 @@ public class GetAllProfileResponseBody {
     }
 
     public static class Profile {
+        private Long id;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
         private Level level;
         private Category category;
         private WorkingType workingType;
