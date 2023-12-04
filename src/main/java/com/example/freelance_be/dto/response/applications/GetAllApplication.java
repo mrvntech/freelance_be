84f652ad1094e38f164dc3bedd7d3326a059234e
mrvntech <mrvntech@gmail.com>
2023-12-04 15:26:48 +0700

    fix bug

--- src/main/java/com/example/freelance_be/dto/response/applications/GetAllApplication.java
+++ src/main/java/com/example/freelance_be/dto/response/applications/GetAllApplication.java
@@ -49,6 +49,15 @@ public class GetAllApplication {
 
     public static class User {
         private Long id;
+        private String fullName;
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
 
         public Long getId() {
             return id;
