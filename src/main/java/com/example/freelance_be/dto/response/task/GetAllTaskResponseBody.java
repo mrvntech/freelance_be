3ff93402124406c590a18b8d7ea627b7d93ac75b
mrvntech <mrvntech@gmail.com>
2023-12-04 22:06:01 +0700

    fix task view

--- src/main/java/com/example/freelance_be/dto/response/task/GetAllTaskResponseBody.java
+++ src/main/java/com/example/freelance_be/dto/response/task/GetAllTaskResponseBody.java
@@ -17,6 +17,15 @@ public class GetAllTaskResponseBody {
         private Long id;
         private String status;
         private Job job;
+        private User user;
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
 
         public Job getJob() {
             return job;
@@ -44,6 +53,87 @@ public class GetAllTaskResponseBody {
     }
     public static class Job {
         private Long id;
+        private String name;
+        private Double budget;
+        private String information;
+        private String imageUrl;
+        private String status;
+        private String createAt;
+        private Category category;
+        private WorkingType workingType;
+        private Level level;
+
+        public Category getCategory() {
+            return category;
+        }
+
+        public void setCategory(Category category) {
+            this.category = category;
+        }
+
+        public WorkingType getWorkingType() {
+            return workingType;
+        }
+
+        public void setWorkingType(WorkingType workingType) {
+            this.workingType = workingType;
+        }
+
+        public Level getLevel() {
+            return level;
+        }
+
+        public void setLevel(Level level) {
+            this.level = level;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Double getBudget() {
+            return budget;
+        }
+
+        public void setBudget(Double budget) {
+            this.budget = budget;
+        }
+
+        public String getInformation() {
+            return information;
+        }
+
+        public void setInformation(String information) {
+            this.information = information;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getCreateAt() {
+            return createAt;
+        }
+
+        public void setCreateAt(String createAt) {
+            this.createAt = createAt;
+        }
 
         public Long getId() {
             return id;
@@ -51,6 +141,96 @@ public class GetAllTaskResponseBody {
 
         public void setId(Long id) {
             this.id = id;
+        }
+    }
+
+    public static class User {
+        private Long id;
+        private String fullName;
+        private String email;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+    public static class WorkingType {
+        private Long id;
+        private String name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    public static class Category {
+        private Long id;
+        private String name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    public static class Level {
+        private Long id;
+        private String name;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 }
